a754ff2890e3925c1f8aa268237982143244ba5a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-02 22:48:52 +0800

    Create Auto_join_TF.js

---
+++ TestFlight/Auto_join_TF.js
@@ -0,0 +1,81 @@
+/*************************************
+
+项目名称：自动加入TF
+引用链接：https://raw.githubusercontent.com/DecoAri/JavaScript/main/Surge/Auto_join_TF.js
+脚本作者：DecoAri
+
+**************************************
+
+
+
+
+[task_local]
+*/20 * * * ? https://raw.githubusercontent.com/chxm1023/Task/main/TestFlight/Auto_join_TF.js, tag=自动加入TF, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/testflight.png, enabled=true
+
+*************************************/
+
+
+!(async () => {
+ids = $persistentStore.read('APP_ID')
+if (ids == '') {
+  $notification.post('所有TF已加入完毕','请手动禁用该模块','')
+} else {
+  ids = ids.split(',')
+  for await (const ID of ids) {
+    await autoPost(ID)
+  }
+}
+$done()
+})();
+
+function autoPost(ID) {
+  let Key = $persistentStore.read('key')
+  let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
+  let header = {
+    'X-Session-Id': `${$persistentStore.read('session_id')}`,
+    'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
+    'X-Request-Id': `${$persistentStore.read('request_id')}`,
+    'User-Agent': `${$persistentStore.read('tf_ua')}`,
+  }
+  return new Promise(function(resolve) {
+    $httpClient.get({url: testurl + ID,headers: header}, function(error, resp, data) {
+      if (error === null) {
+        if (resp.status == 404) {
+          ids = $persistentStore.read('APP_ID').split(',')
+          ids = ids.filter(ids => ids !== ID)
+          $persistentStore.write(ids.toString(),'APP_ID')
+          console.log(ID + ' ' + '不存在该TF，已自动删除该APP_ID')
+          $notification.post(ID, '不存在该TF', '已自动删除该APP_ID')
+          resolve()
+        } else {
+          let jsonData = JSON.parse(data)
+          if (jsonData.data == null) {
+            console.log(ID + ' ' + jsonData.messages[0].message)
+            resolve();
+          } else if (jsonData.data.status == 'FULL') {
+            console.log(jsonData.data.app.name + ' ' + ID + ' '+ jsonData.data.message)
+            resolve();
+          } else {
+            $httpClient.post({url: testurl + ID + '/accept',headers: header}, function(error, resp, body) {
+              let jsonBody = JSON.parse(body)
+              $notification.post(jsonBody.data.name, 'TestFlight加入成功', '')
+              console.log(jsonBody.data.name + ' TestFlight加入成功')
+              ids = $persistentStore.read('APP_ID').split(',')
+              ids = ids.filter(ids => ids !== ID)
+              $persistentStore.write(ids.toString(),'APP_ID')
+              resolve()
+            });
+          }
+        }
+      } else {
+        if (error =='The request timed out.') {
+          resolve();
+        } else {
+          $notification.post('自动加入TF', error,'')
+          console.log(ID + ' ' + error)
+          resolve();
+        }
+      }
+    })
+  })
+}
