81cf40e187e6afcc0a3b5c45cbbdb12925d7e317
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-02 23:11:33 +0800

    Update Auto_join_TF.js

--- TestFlight/Auto_join_TF.js
+++ TestFlight/Auto_join_TF.js
@@ -7,12 +7,13 @@
 **************************************
 
 [task_local]
+# # 自动加入TF(60分钟执行一次)
 */60 * * * ? https://raw.githubusercontent.com/chxm1023/Task/main/TestFlight/Auto_join_TF.js, tag=自动加入TF, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/testflight.png, enabled=true
 
 [rewrite_local]
 # 获取TF信息
 ^https?:\/\/testflight\.apple\.com\/v3\/accounts/.*\/apps$ url script-request-header https://raw.githubusercontent.com/chxm1023/Task/main/TestFlight/TF_keys.js
-# 获取APP_ID
+# APP_ID获取
 ^https?:\/\/testflight\.apple\.com\/join\/(.*) url script-request-header https://raw.githubusercontent.com/chxm1023/Task/main/TestFlight/TF_keys.js
 
 [MITM]
